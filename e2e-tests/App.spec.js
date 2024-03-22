5bad6ac9c9e3fe256f41673b59a4541926c0d8a7
Danjali <46020764+Danjali@users.noreply.github.com>
2024-03-22 12:18:14 +0530

    Add files via upload

--- e2e-tests/App.spec.js
+++ e2e-tests/App.spec.js
@@ -1,1 +1,10 @@
+import { test } from "../fixtures/app-fixtures";
 
+const { expect } = require("@playwright/test");
+
+// const { test, expect } = require('../../baseFixtures');
+test("Home page", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await page.waitForTimeout(3000);
+  await expect(page).toHaveTitle("React App");
+});
