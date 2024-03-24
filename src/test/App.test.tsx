b9e459566ee8bd7082bdd6728b57f567ad739e8e
Danjali <46020764+Danjali@users.noreply.github.com>
2024-03-24 11:54:12 +0530

    Update App.test.tsx

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+/* eslint-disable testing-library/prefer-screen-queries */
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Mock Title component
+jest.mock('../utils/Title', () => () => <div data-testid="mock-title">Mock Title</div>);
+describe('App Component', () => {
+  test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test('renders Title component with correct props', () => {
+    const { getByTestId } = render(<App />);
+    const titleElement = getByTestId('mock-title');
+    expect(titleElement).toBeInTheDocument();
+  });
+  test('clicking button calls getAlert function', () => {
+    const { getByTestId } = render(<App />);
+    const buttonElement = getByTestId('myButton');
+    const alertSpy = jest.spyOn(window, 'alert').mockImplementation(() => { });
+    fireEvent.click(buttonElement);
+    expect(alertSpy).toHaveBeenCalledWith('sum is 30');
+    alertSpy.mockRestore();
+  });
 });
