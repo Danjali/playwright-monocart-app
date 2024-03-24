68b4f56bfbe1b57ef868f0757b65d2d90377aa21
Danjali <46020764+Danjali@users.noreply.github.com>
2024-03-24 11:49:44 +0530

    Update mergeReports.js

--- mergeReports.js
+++ mergeReports.js
@@ -1,29 +1,32 @@
 // esm syntax
-// import fs from 'fs';
-// import { CoverageReport } from 'monocart-coverage-reports';
-const EC = require('eight-colors');
-const MCR = require('monocart-coverage-reports');
+const EC = require("eight-colors");
+const MCR = require("monocart-coverage-reports");
+const fs = require("fs");
 const coverageOptions = {
-  name: 'My Merged Coverage Report',
-  inputDir: ['./coverage-results/raw', ./testresults/ct/raw'],
-  outputDir: './coverage-reports/merged',
-  reports: [['v8'], ['console-summary']],
-  // onEnd: () => {
-  //   // remove the raw files if it useless
-  //   fs.rmSync('./coverage-reports/unit/raw', {
-  //     recursive: true,
-  //     force: true,
-  //   });
-  // },
+  name: "Merged Coverage Report",
+  inputDir: [
+    "./jest-coverage-results/raw",
+    "./playwright-coverage-results/ct/raw",
+  ],
+  outputDir: "./merged-coverage-reports/merged",
+  reports: [["v8"], ["console-summary"]],
+  onEnd: () => {
+    // remove the raw files if it useless
+    fs.rmSync("./coverage-reports/rere", {
+      recursive: true,
+      force: true,
+    });
+  },
 };
 
 const generate = async () => {
   // clean cache first
   await MCR(coverageOptions).cleanCache();
-  console.log('generate merge coverage reports ...');
+  console.log("generate merge coverage reports ...");
   const coverageResults = await MCR(coverageOptions).generate();
-  console.log('merge coverage reportPath', EC.magenta(coverageResults.reportPath));
+  console.log(
+    "merge coverage reportPath",
+    EC.magenta(coverageResults.reportPath)
+  );
 };
 generate();
-// console.log('dds');
-// await new MCR(coverageOptions).generate();
